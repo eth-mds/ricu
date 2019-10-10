c67fbe56110df8634c0d28e7a84ba242cb3099b0
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2019-10-10 16:34:35 +0200

    add dummy test

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,14 @@ library(testthat)
 library(sepsr)
 
 if (requireNamespace("xml2")) {
-  test_check("sepsr", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
+  test_check(
+    "sepsr",
+    reporter = MultiReporter$new(
+      reporters = list(JunitReporter$new(file = "test-results.xml"),
+                       CheckReporter$new()
+      )
+    )
+  )
 } else {
   test_check("sepsr")
 }
