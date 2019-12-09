d80efad291f9dea89161fa13e51c2f8a32d38120
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2019-12-09 14:25:26 +0100

    surface mimic_icustays()

--- R/mimic-tables.R
+++ R/mimic-tables.R
@@ -8,6 +8,33 @@ mimic_admissions <- function(cols = character(0L), rows = NULL,
 }
 
 #' @export
+mimic_icustays <- function(cols = character(0L), rows = NULL,
+                           id_cols = "hadm_id", time_col = "intime",
+                           ...) {
+
+  mimic_ts_quo("icustays", rows, cols, id_cols, time_col, ...)
+}
+
+#' @export
+mimic_input_mv <- function(cols = character(0L), rows = NULL,
+                           id_cols = "hadm_id", time_col = "starttime", ...) {
+
+  val_cols  <- c("amount",    "rate",    "totalamount")
+  unit_cols <- c("amountuom", "rateuom", "totalamountuom")
+
+  mimic_ts_unit_quo("inputevents_mv", rows, cols, id_cols, time_col,
+                     ..., val_cols = val_cols, unit_cols = unit_cols)
+}
+
+#' @export
+mimic_lab <- function(cols = character(0L), rows = NULL, id_cols = "hadm_id",
+                      time_col = "charttime", ...) {
+
+  mimic_ts_unit_quo("labevents", rows, cols, id_cols, time_col,
+                    ..., val_cols = "valuenum", unit_cols = "valueuom")
+}
+
+#' @export
 mimic_microbio <- function(cols = character(0L), rows = NULL,
                            id_cols = "hadm_id", time_col = "charttime",
                            ...) {
@@ -29,15 +56,4 @@ mimic_prescriptions <- function(cols = character(0L), rows = NULL,
   date_inds <- paste0(date_cols, "_date")
 
   update_ts_def(res, new_ts_date(date_cols))
-}
-
-#' @export
-mimic_input_mv <- function(cols = character(0L), rows = NULL,
-                           id_cols = "hadm_id", time_col = "starttime", ...) {
-
-  val_cols  <- c("amount",    "rate",    "totalamount")
-  unit_cols <- c("amountuom", "rateuom", "totalamountuom")
-
-  mimic_ts_unit_quo("inputevents_mv", rows, cols, id_cols, time_col,
-                     ..., val_cols = val_cols, unit_cols = unit_cols)
 }
