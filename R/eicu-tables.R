4bab8c774147f875a67361545aa457901733d375
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2019-12-12 14:50:15 +0100

    expose eicu_lab()

--- R/eicu-tables.R
+++ R/eicu-tables.R
@@ -1,5 +1,18 @@
 
 #' @export
+eicu_lab <- function(cols = character(0L), rows = NULL,
+                     id_cols = "patienthealthsystemstayid",
+                     time_col = "labresultoffset", ...) {
+
+  # it might make sense to pool the cols `labmeasurenamesystem` and
+  # `labmeasurenameinterface` for units
+
+  eicu_ts_unit_quo("lab", rows, cols, id_cols, time_col, ...,
+                   val_cols = "labresult",
+                   unit_cols = "labmeasurenameinterface")
+}
+
+#' @export
 eicu_medication <- function(cols = character(0L), rows = NULL,
                             id_cols = "patienthealthsystemstayid",
                             time_col = "drugstartoffset", ...) {
