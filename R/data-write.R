5fa035863c7fb2ab0e45a9ecaa7cf41abdfd1614
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-04-15 19:30:39 +0200

    add na flexibility to psv export

--- R/data-write.R
+++ R/data-write.R
@@ -1,11 +1,13 @@
 
 #' @export
-write_psv <- function(x, dir, na_rm = TRUE) {
+write_psv <- function(x, dir, na_rows = NULL) {
 
-  assert_that(is.flag(na_rm))
-
-  if (na_rm) {
+  if (isTRUE(na_rows)) {
+    x <- fill_gaps(x)
+  } else if (isFALSE(na_rows)) {
     x <- rm_na(x, cols = data_cols(x), mode = "all")
+  } else {
+    assert_that(is.null(na_rows))
   }
 
   dat <- split(x, by = id(x))
