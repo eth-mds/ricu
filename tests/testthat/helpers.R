02bef3279116f1d532d11b308570d57429c380c1
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2020-12-09 11:28:04 +0100

    split test helpers into def/tests

---
+++ tests/testthat/helpers.R
@@ -0,0 +1,105 @@
+
+expect_all_identical <- function(object, expected = object[[1L]], ...,
+                                 info = NULL, label = NULL,
+                                 expected_label = NULL) {
+
+  act <- quasi_label(rlang::enquo(object), label, arg = "object")
+  exp <- quasi_label(rlang::enquo(expected), expected_label,
+                     arg = "expected")
+
+  if (length(exp$val) == 1L) {
+    ident <- vapply(act$val, identical, logical(1L), exp$val, ...)
+  } else {
+    ident <- mapply(identical, act$val, exp$val, MoreArgs = list(...))
+  }
+
+  if (all(ident)) {
+
+    msg <- ""
+
+  } else {
+
+    if (length(exp$val) == 1L) {
+      comp <- lapply(act$val[!ident], compare, exp$val)
+    } else {
+      comp <- Map(compare, act$val[!ident], exp$val[!ident])
+    }
+
+    msgs <- lapply(comp, function(co) {
+      if (co$equal) {
+        msg <- "Objects equal but not identical"
+      } else {
+        msg <- co$message
+      }
+    })
+
+    msg <- paste(msgs, collapse = "\n  ")
+  }
+
+  expect(all(ident), sprintf("%s not identical to %s.\n  %s", act$lab,
+                             exp$lab, msg), info = info)
+
+  invisible(act$val)
+}
+
+expect_all_equal <- function(object, expected = object[[1L]], ..., info = NULL,
+                             label = NULL, expected_label = NULL) {
+
+  act <- quasi_label(rlang::enquo(object), label, arg = "object")
+  exp <- quasi_label(rlang::enquo(expected), expected_label,
+                     arg = "expected")
+
+  if (length(exp$val) == 1L) {
+    comp <- lapply(act$val, compare, exp$val)
+  } else {
+    comp <- Map(compare, act$val, exp$val)
+  }
+
+  equ <- vapply(comp, `[[`, logical(1L), "equal")
+  msg <- vapply(comp, `[[`, character(1L), "message")
+
+  msg <- paste(msg[!equ], collapse = "\n  ")
+
+  expect(all(equ), sprintf("%s not identical to %s.\n  %s", act$lab,
+                           exp$lab, msg), info = info)
+
+  invisible(act$val)
+}
+
+expect_all <- function(object, info = NULL, label = NULL) {
+
+  act <- quasi_label(rlang::enquo(object), label, arg = "object")
+
+  expect(all(as.logical(act$val)),
+         sprintf("%s contains non-true entries.", act$lab), info = info)
+
+  invisible(act$val)
+}
+
+expect_fsetequal <- function(object, expected, ...) {
+
+  act <- quasi_label(rlang::enquo(object))
+  exp <- quasi_label(rlang::enquo(expected))
+
+  expect(
+    data.table::fsetequal(act$val, exp$val, ...),
+    sprintf("%s is not fsetequal to %s.", act$lab, exp$lab)
+  )
+
+  invisible(act$val)
+}
+
+skip_if_srcs_missing <- function(srcs) {
+
+  avail <- is_data_avail(srcs)
+  skip  <- !all(avail)
+
+  if (skip) {
+    msg <- fmt_msg("Data source{?s} {quote_bt(srcs[!avail])} {?is/are} missing
+                    but required for tests")
+  } else {
+    msg <- NULL
+  }
+
+  skip_if(skip, msg)
+}
