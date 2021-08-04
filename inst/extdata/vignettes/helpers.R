6e3fbf18cce2db83222715b74c1d5a57cb464735
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2021-08-04 15:50:33 +0200

    vignette fixes

--- inst/extdata/vignettes/helpers.R
+++ inst/extdata/vignettes/helpers.R
@@ -1,4 +1,5 @@
-srcs_avail <- function(x) all(is_data_avail(x))
+
+srcs_avail <- function(x) all(ricu::is_data_avail(x))
 
 paste1 <- function(x) {
 
@@ -12,44 +13,46 @@ paste1 <- function(x) {
          paste0(tail(x, n = 2L), collapse = " and "))
 }
 
-demo_instead_full_msg <- function(demo, full) {
+demo_instead_full_msg <- function(demo, full, file) {
   cat(
     "> Note: The following code blocks are run using demo (",
-    paste1(demo), ", ) instead of full datasets (", paste1(full), ", ) and ",
+    paste1(demo), ") instead of full (", paste1(full), ") datasets and ",
     "therefore might be less useful. For data set up, please consult the ",
     "manual at `?attach_src`. The full version of this vignette is available ",
-    "from [CRAN](https://CRAN.R-project.org/package=ricu/vignettes/uom.html).",
-    sep = ""
+    "from [CRAN](https://CRAN.R-project.org/package=ricu/vignettes/", file,
+    ").", sep = ""
   )
 }
 
-demo_missing_msg <- function(demo) {
+demo_missing_msg <- function(demo, file) {
   cat(
     "> Note: Code in this vignette requires that datasets ", paste1(demo),
     " are available, which can be installed by running\n",
     ">\n",
-    ">     install.packages(\n",
-    ">       c(", paste0("\"", sub("_", ".", demo), "\"", collapse = ", "),
-             "),\n",
-    ">       repos = \"https://eth-mds.github.io/physionet-demo\"\n",
-    ">     )\n",
+    "> ```r\n",
+    "> install.packages(\n",
+    ">   c(", paste0("\"", sub("_", ".", demo), "\"", collapse = ", "), "),\n",
+    ">   repos = \"https://eth-mds.github.io/physionet-demo\"\n",
+    "> )\n",
+    "> ```\n",
     ">\n",
     "> As long as the corresponding datasets are not accessible, certain code ",
     "blocks are not evaluated. The full version of this vignette is available ",
-    "from [CRAN](https://CRAN.R-project.org/package=ricu/vignettes/uom.html).",
-    sep = ""
+    "from [CRAN](https://CRAN.R-project.org/package=ricu/vignettes/", file,
+    ").", sep = ""
   )
 }
 
 assign_dataset_names <- function(srcs) {
   name <- deparse(substitute(srcs))
   cat(
-    "```{r, assign-", name, ", eval = FALSE}\n",
-    name, " <- c(", paste0("\"", srcs, "\"", collapse = ", "), ")\n",
-    "```",
+    "```r\n",
+    name, " <- ", capture.output(dput(srcs)), "\n",
+    "```\n",
     sep = ""
   )
 }
 
-demo <- c("mimic_demo", "eicu_demo")
-srcs <- c("mimic", "eicu", "aumc", "hirid", "miiv")
+combine_chunks <- function(x) {
+  gsub("```\n*```r*\n*", "\n", paste0(x, collapse = "\n"))
+}
