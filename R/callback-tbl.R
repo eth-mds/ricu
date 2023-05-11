d459959c85853dec2c703569f27a80409b2e2f54
prockenschaub <rockenschaub.patrick@gmail.com>
2023-05-11 07:24:28 +0200

    add callback to deserialise sicdb data_float_h

---
+++ R/callback-tbl.R
@@ -0,0 +1,24 @@
+
+sic_data_float_h <- function(dat, ...) {
+  hexstring_to_float <- function(x) {
+    if (is.na(x)) {
+      return(NA_real_)
+    }
+    hexstring <- substring(x, seq(1, 482, 2), seq(2, 482, 2))
+    bytes <- as.raw(strtoi(hexstring[-1], base = 16))
+    floats <- readBin(bytes, numeric(), length(bytes) %/% 4, 4, endian = "little")
+    ifelse(floats == 0, NA_real_, floats)
+  }
+  
+  setDT(dat)
+  dat[, c("rawdata") := lapply(get("rawdata"), hexstring_to_float)] # TODO: remove hard coding of rawdata and derive from JSON config
+  dat <- dat[, .(
+    Offset = Offset + 60 * (0:(sapply(rawdata, length)-1)),
+    Val = Val,
+    cnt = cnt,
+    rawdata = unlist(rawdata)
+  ),
+  by = .(id, CaseID, DataID)
+  ]
+  dat
+}
